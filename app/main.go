caf27d067ddaca0df3e82d841748de7adab80bb6
ChothaninThitisrirat <maithong31.mmm@gmail.com>
2026-05-05 14:24:08 +0700

    Print load .env file

--- app/main.go
+++ app/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/chothanin01/PhoSS-Care-server/configs"
@@ -12,7 +13,7 @@ import (
 
 func main() {
 	if err := godotenv.Load(); err != nil {
-		log.Fatal("Error loading .env file")
+		fmt.Println("Error loading .env file")
 	}
 
 	appconfig := configs.LoadConfig()
